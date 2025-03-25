a5f6f7af3c4653515bf955dc7ceb706e8660edec
Raz <raz.sakal30@gmail.com>
2025-03-25 19:40:50 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,5 @@
-import os
-import sys
 import pytest
-# Add the directory containing api_user_fetcher.py to the PYTHONPATH
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from api_user_fetcher import UserFetcher
+from ..api_user_fetcher import UserFetcher
 
 
 @pytest.fixture(scope="module")
