b23b6b8dcc5f5a6690d21e3539a6e81cf09bdc8b
Raz <raz.sakal30@gmail.com>
2025-03-27 12:13:51 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,10 @@ import pytest
 import os
 import sys
 import pytest
+from api_user_fetcher import UserFetcher
 
 # Add the parent directory of the tests directory to the PYTHONPATH
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from api_user_fetcher import UserFetcher
-
 
 @pytest.fixture(scope="module")
 def user_fetcher():
